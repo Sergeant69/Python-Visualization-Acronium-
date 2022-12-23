322f157d410ae7e7493322be8cb562ec4f422f52
Sergeant69 <121296381+Sergeant69@users.noreply.github.com>
2022-12-23 18:25:42 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -4,13 +4,13 @@ from random import randint
 speed(0)
 bgcolor("black")
 x =1 
-while x < 40:
+while x < 800:
     r= randint (0,255)
     g= randint (0,255)
     b= randint (0,255)
     colormode(255)
     pencolor(r,g,b)
-    fd(50+x)
+    fd(90+x)
     rt(90.911)
     x += 1
 exitonclick()
